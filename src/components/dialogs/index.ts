44a1426aa3aeb58836253aa891292632b79296e4
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2023-11-08 15:47:17 -0500

    initial release

--- src/components/dialogs/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DialogClusterSettings } from './DialogClusterSettings.vue';
-export { default as DialogWalletRequired } from './DialogWalletRequired.vue';
-export { default as DialogWalletIssue } from './DialogWalletIssue.vue';
--- src/components/elements/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Layout } from './Layout.vue';
-export { default as Button } from './Button.vue';
-export { default as Logo } from './Logo.vue';
